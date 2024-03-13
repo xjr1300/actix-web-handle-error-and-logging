162cae8fc819c7b3d9d8d5d4eb0998463246d0d6
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-03-13 10:41:40 +0900

    Implement the request handler for health check.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(health_check)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+/// ヘルス・チェック
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("It works!")
 }
